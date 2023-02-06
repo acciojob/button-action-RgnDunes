6852f5cc4747e2923545409f0c25394761cd9847
RgnDunes <divyansh.singh@razorpay.com>
2023-02-06 22:48:34 +0530

    Add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
   return (
     <div id="main">
+      <button>Click me</button>
       // Do not alter the main div
     </div>
   );
 }
-
 
 export default App;
