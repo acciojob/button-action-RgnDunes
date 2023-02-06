6399e2c213b9f120b773ccea9b6deeb6860b771e
RgnDunes <divyansh.singh@razorpay.com>
2023-02-06 22:54:57 +0530

    Add solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div id="main">
-      <p>This is a paragraph</p>
-      <button id="button">Click me</button>
+    <div id="main" className="bg-pink">
+      {isClicked && (
+        <p>
+          <h1>This is a paragraph</h1>
+        </p>
+      )}
+      <button id="button" onClick={handleClick}>
+        Click me
+      </button>
       // Do not alter the main div
     </div>
   );
